0c90a1cc68ab03c399f3e08716dd5f8b3b104077
@hexlet/cli <support@hexlet.io>
2023-04-23 15:56:32 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -1,9 +1,10 @@
 package exercise;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.List;
 import java.util.Arrays;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class AppTest {
@@ -11,23 +12,31 @@ class AppTest {
     @Test
     void testTake1() {
         // BEGIN
-        List<Integer> testWrong1 = Arrays.asList(null, 5);
-        assertThat(testWrong1).isEqualTo(testWrong1);
+        List<Integer> num1 = new ArrayList<>();
+        List<Integer> result1 = App.take(num1, 5);
+        assertThat(result1).hasSize(0);
     }
+
     @Test
     void testTake2() {
-        List<Integer> testRight = Arrays.asList(5, 5);
-        assertThat(testRight).isEqualTo(testRight);
+        List<Integer> num2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        List<Integer> result2 = App.take(num2, 5);
+        List<Integer> expected2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        assertThat(result2).isEqualTo(expected2);
     }
     @Test
     void testTake3() {
-        List<Integer> testWrong2 = Arrays.asList(2, 3);
-        assertThat(testWrong2).isEqualTo(testWrong2);
+        List<Integer> num3 = new ArrayList<>(Arrays.asList(1, 2));
+        List<Integer> result3 = App.take(num3, 3);
+        List<Integer> expected3 = new ArrayList<>(Arrays.asList(1, 2));
+        assertThat(result3).isEqualTo(expected3);
     }
     @Test
     void testTake4() {
-        List<Integer> testWrong3 = Arrays.asList(3, 2);
-        assertThat(testWrong3).isEqualTo(testWrong3);
+        List<Integer> num4 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> result4 = App.take(num4, 3);
+        List<Integer> expected4 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        assertThat(result4).isEqualTo(expected4);
         // END
     }
 }
